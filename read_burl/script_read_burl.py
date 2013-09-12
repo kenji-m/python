0c935b99a307703498fe05ea4e3c59fd500eefae
kenji-m <kenji.miyajima@gmail.com>
2013-09-12 13:53:29 -0500

    HW 1, Part1: Script

--- read_burl/script_read_burl.py
+++ read_burl/script_read_burl.py
@@ -32,8 +32,8 @@ for line in f.readlines()[2:]:
     
     dates.append ( datetime(year, month, day, hour, minute) )
     pressure.append ( float(data[12]) )
-    u_wind.append ( float(data[6]) * np.sin(float(data[5])*np.pi/180)*-1)
-    v_wind.append ( float(data[6]) * np.cos(float(data[5])*np.pi/180)*-1)
+    u_wind.append ( -(float(data[6])) * np.sin(float(data[5])*np.pi/180) )
+    v_wind.append ( -(float(data[6])) * np.cos(float(data[5])*np.pi/180) )
 
 data = {'dates': np.array(dates), 'pressure': np.array(pressure), 
         'u-wind': np.array(u_wind), 'v-wind': np.array(v_wind)}
